233dbcbe79e173f24002782a00b4fa17eb1601bb
cromarmot <yexiaorain@gmail.com>
2021-02-18 17:34:10 +0800

    chores: update for demo

--- src/app/for-demo/for-demo.component.ts
+++ src/app/for-demo/for-demo.component.ts
@@ -1,25 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-for-demo',
   templateUrl: './for-demo.component.html',
   styleUrls: ['./for-demo.component.scss'],
 })
-export class ForDemoComponent implements OnInit {
+export class ForDemoComponent {
   name = '';
   newId = 3;
   list = [
-    { id: 1, name: '李斯' },
-    { id: 2, name: '吕不韦' },
-    { id: 3, name: '嬴政' },
+    { id: 1, name: 'Name0' },
+    { id: 2, name: 'Name1' },
+    { id: 3, name: 'Name2' },
   ];
 
-  constructor() {}
-
-  ngOnInit(): void {}
-
   add(): void {
-    // 注意这里是unshift
     this.list.unshift({
       id: ++this.newId,
       name: this.name,
