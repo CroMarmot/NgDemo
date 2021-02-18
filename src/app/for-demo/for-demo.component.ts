142b6c1f807b1d1464e4f4ab86fe49d2a7208037
cromarmot <yexiaorain@gmail.com>
2021-02-18 11:18:38 +0800

    chore: husky && ng add @angular/pwa

---
+++ src/app/for-demo/for-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-for-demo',
+  templateUrl: './for-demo.component.html',
+  styleUrls: ['./for-demo.component.scss']
+})
+export class ForDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
